ec176e735fa81bb58d41def486fff120fbebc29a
aitkhsh <aitkhsh7@yahoo.co.jp>
2024-10-06 13:40:24 +0900

    add: github外部認証機能の導入

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,27 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+
+  def oauth
+    login_at(params[:provider])
+  end
+
+  # def oauth
+  #   redirect_to sorcery_login_url(params[:provider]), allow_other_host: true
+  # end
+
+  def callback
+    provider = params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+    else
+      begin
+        @user = create_from(provider)
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+      rescue
+        redirect_to root_path, error: "#{provider.titleize}アカウントでのログインに失敗しました。"
+      end
+    end
+  end
+end
