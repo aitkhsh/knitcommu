f68dd4d3d3d3aaa848796547b0bbb96ecba2678b
aitkhsh <aitkhsh7@yahoo.co.jp>
2024-10-07 18:53:58 +0900

    add: プロフィール作成画面_2

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,4 +6,20 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+
+  def create
+    @profile = current_user.profiles.build(profile_params)
+    if @profile.save
+      redirect_to profiles_path, success: t("defaults.flash_message.created", item: Profile.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Profile.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :body)
+  end
 end
