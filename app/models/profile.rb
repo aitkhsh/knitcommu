78662ca804cdd64120ced7074c28387727191f49
aitkhsh <aitkhsh7@yahoo.co.jp>
2024-10-07 20:59:59 +0900

    add: フォーム入力時エラー

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 65_535 }
+  belongs_to :user
+  
 end
